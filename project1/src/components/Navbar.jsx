4f1d0c9c27d99ef8b38c63658b8e2b63e5fb5af0
githanirz <githanipasya@gmail.com>
2025-07-20 17:12:46 +0700

    update navbar mobile

--- project1/src/components/Navbar.jsx
+++ project1/src/components/Navbar.jsx
@@ -25,41 +25,43 @@ const Header = () => {
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
         <img src={logo} className="h-12 w-60 mr-20" alt="logo" />
-        <div className="dropdown lg:hidden">
-          <div tabIndex={0} role="button" className="btn btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+        {isLoggedIn && (
+          <div className="dropdown lg:hidden">
+            <div tabIndex={0} role="button" className="btn btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/mitra">Mitra</Link>
+              </li>
+              <li>
+                <Link to="/prediksi">Prediksi</Link>
+              </li>
+              <li>
+                <Link to="/riwayat">Riwayat</Link>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/mitra">Mitra</Link>
-            </li>
-            <li>
-              <Link to="/prediksi">Prediksi</Link>
-            </li>
-            <li>
-              <Link to="/riwayat">Riwayat</Link>
-            </li>
-          </ul>
-        </div>
+        )}
       </div>
 
       {isLoggedIn && (
